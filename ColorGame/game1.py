d1532e3be516c9208f60583981a873a66d6b0ddd
alyssar60819 <alyssar60819@gmail.com>
2023-02-10 14:06:00 -0800

    api game 1

--- ColorGame/game1.py
+++ ColorGame/game1.py
@@ -30,3 +30,31 @@ def get_score(player_id):
 
 if __name__ == "__main__":
     app.run()
+
+
+# To incorporate the REST API into my game code, I made API calls to update and retrieve the player's score. 
+# The API provides two routes: /score for updating the score and /score/<player_id> for retrieving the score.
+# Here's how I API calls in Python using the requests library:
+
+
+
+import requests
+
+def update_score(player_id, score):
+    response = requests.post("http://localhost:5000/score", data={"player_id": player_id, "score": score})
+    if response.status_code == 200:
+        print("Score updated for player {}".format(player_id))
+    else:
+        print("Failed to update score: {}".format(response.text))
+
+def get_score(player_id):
+    response = requests.get("http://localhost:5000/score/{}".format(player_id))
+    if response.status_code == 200:
+        return int(response.text)
+    elif response.status_code == 404:
+        print("Player not found")
+        return None
+    else:
+        print("Failed to retrieve score: {}".format(response.text))
+        return None
+
